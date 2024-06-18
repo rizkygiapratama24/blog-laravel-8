29242698d81ce8a7f8a12b3a0473f6d58618a369
Rizky Giapratama <rizkygiapratama24@gmail.com>
2024-06-19 00:18:12 +0700

    Upload Vue JS 3 Blog

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//import Bootstrap, Popper, jQuery
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery'
+import 'popper.js/dist/popper'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
 createApp(App).mount('#app')
