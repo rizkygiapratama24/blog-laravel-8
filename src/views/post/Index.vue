c73b03b6f91c2352dd0abc29f9d2f90252ec4f4f
Rizky Giapratama <rizkygiapratama24@gmail.com>
2024-06-25 16:13:23 +0700

    Update Blog (change description)

--- src/views/post/Index.vue
+++ src/views/post/Index.vue
@@ -64,7 +64,7 @@ export default {
                 })
         })
 
-        // search
+        // searched post
         const searchposts = ref("");
         const searchedPosts = computed(() => {
             return posts.value.filter((post) => {
